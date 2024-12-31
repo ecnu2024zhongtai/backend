7502a8e677013eb2245019583d7b6dbbaa631a85
CRUDYYDS <crudyyds@gmail.com>
2024-12-31 12:32:00 +0800

    integrate toparea into webhosting

--- WebHosting/main.py
+++ WebHosting/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, APIRouter, Depends
-from app.api.v1.endpoints import trip
+from app.api.v1.endpoints import trip, toparea
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -21,9 +21,9 @@ def read_root():
     return {"message": "Welcome to the FastAPI app!"}
 
 app.include_router(trip.router, prefix="/api/v1")
-
+app.include_router(toparea.router, prefix="/api/v1")
 
 import os
 
-port = int(os.environ.get("PORT", 8004))
+port = int(os.environ.get("PORT", 8005))
 uvicorn.run(app, host="::", port=port)
